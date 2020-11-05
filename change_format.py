e7b6feefcb96f403c5ba87ab99f21fce48bb8069
Deepak Rai <deepakjmk10@gmail.com>
2020-11-05 12:07:12 +0900

    Update and rename 2.py to change_format.py

---
+++ change_format.py
@@ -0,0 +1,19 @@
+#Example of Changing the files format for pre-processing
+
+import os
+import cv2
+
+print(len(os.listdir("./folder")))
+
+pngs = os.listdir('./folder')
+print(pngs)
+for j in pngs:
+    #img = cv2.imread(j)
+    #cv2.imwrite(j[:-3] + 'jpg', img)
+    os.rename("./folder/" + j, "./folder/" + j[:-3] + 'jpg')
+
+print(os.listdir("./folder"))
+            
+
+
+
